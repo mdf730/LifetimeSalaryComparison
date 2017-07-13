8347203b62a0492887b85e78ba5bd28b86285804
Matthew Fabian <mfabian1@Matthews-MacBook-Pro.local>
2017-07-12 21:46:03 -0400

    added 401K math

--- run.py
+++ run.py
@@ -27,25 +27,25 @@ offer1 = form.Form(
         placeholder="3%"))
 
 offer2 = form.Form(
-	form.Textbox("Salary",
+	form.Textbox("Salary 2",
 		form.notnull,
         form.regexp('\d+', 'Must be a digit'),
         placeholder="$35,000"), 
-    form.Textbox("Annual Raise", 
+    form.Textbox("Annual Raise 2", 
         form.notnull,
         form.regexp('\d+', 'Must be a digit'),
         placeholder="3.5%"),
-    form.Textbox("401K Contribution", 
+    form.Textbox("401K Contribution 2", 
         form.notnull,
         form.regexp('\d+', 'Must be a digit'),
         placeholder="6%"),
-    form.Textbox("401K Match", 
+    form.Textbox("401K Match 2", 
         form.notnull,
         form.regexp('\d+', 'Must be a digit'),
         placeholder="3%"))
 
 details = form.Form(
-	form.Textbox("Current Age",
+	form.Textbox("Age",
     	form.notnull,
         form.regexp('\d+', 'Must be a digit'),
         form.Validator(' Must be less than 65', lambda x:int(x)<65)),
@@ -68,31 +68,56 @@ class index:
     	form3 = details()
     	if not offer1.validates():
     		return render.formtest(form1, form2, form3)
+    	elif not offer2.validates():
+    		return render.formtest(form1, form2, form3)
+    	elif not details.validates():
+    		return render.formtest(form1, form2, form3)
     	else:
-    		lowerSalary = form["Lower Salary"].value
-    		higherSalary = form["Higher Salary"].value
-    		currentAge = form["Current Age"].value
-    		annualRaise = form["Annual Raise"].value
+    		salary1 = offer1["Salary"].value
+    		salary2 = offer2["Salary 2"].value
+    		currentAge = details["Age"].value
+    		raise1 = offer1["Annual Raise"].value
+    		raise2 = offer2["Annual Raise 2"].value
+    		contribution1 = offer1["401K Contribution"].value
+    		contribution2 = offer2["401K Contribution 2"].value
+    		match1 = offer1["401K Match"].value
+    		match2 = offer2["401K Match 2"].value
+    		market = details["Market growth"].value
 
     		career = 66 - int(currentAge)
+
     		totalLow = 0
     		totalHigh = 0
-    		lowYear = float(lowerSalary)
-    		highYear = float(higherSalary)
-    		for year in range(career):
-    			totalLow = totalLow + lowYear
-    			totalHigh = totalHigh + highYear
-    			lowYear = (lowYear * (100 + float(annualRaise)))/100
-    			highYear = (highYear * (100 + float(annualRaise)))/100
 
-    		difference = totalHigh - totalLow
-    		return '''
-    		Lower: %s
-    		Higher: %s
-    		Age: %s
-    		Raise: %s
-    		Extra Earnings: %s
-    		''' % (lowerSalary, higherSalary, currentAge, annualRaise, difference)
+    		totalRetireLow = 0
+    		totalRetireHigh = 0
+    		
+    		lowYear = float(salary2)
+    		highYear = float(salary1)
+
+    		retireLowYear = float(salary2)*(float(contribution2)+float(match2))/100
+    		retireHighYear = float(salary1)*(float(contribution1)+float(match1))/100
+    		
+    		for year in range(career):
+    			# FORM 1
+    			totalHigh = totalHigh + highYear*(100 - float(contribution1))/100
+    			highYear = (highYear * (100 + float(raise1)))/100
+    			totalRetireHigh = totalRetireHigh*(100 + float(market))/100 + retireHighYear
+    			retireHighYear = highYear * (float(contribution1)+float(match1))/100
+    			
+    			# FORM 2
+    			totalLow = totalLow + lowYear*(100 - float(contribution2))/100
+    			lowYear = (lowYear * (100 + float(raise2)))/100
+    			totalRetireLow = totalRetireLow*(100 + float(market))/100 + retireLowYear
+    			retireLowYear = lowYear * (float(contribution2)+float(match2))/100
+
+    			print totalHigh,
+    			print totalLow,
+    			print totalRetireHigh,
+    			print totalRetireLow
+
+    		difference = totalHigh + totalRetireHigh - totalLow - totalRetireLow
+    		return render.formresults(difference)
 
 if __name__ == "__main__":
 	app = web.application(urls, globals())
